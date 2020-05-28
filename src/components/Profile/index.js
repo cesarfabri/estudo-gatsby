e22bb27be3408513399936c578eb227f9345a89c
Cesar Fabri <cesar.fabrii@gmail.com>
2020-05-28 13:11:35 -0300

    primeira confirmação

---
+++ src/components/Profile/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Profile = () => (
+  <div className="Profile-wrapper">
+    <h1>Cesar Fabri</h1>
+    <h2>Programador de Backend</h2>
+    <p>CTO da DI Center, responsavel pela tecnologia adotada pela empresa.</p>
+  </div>
+)
+
+export default Profile
